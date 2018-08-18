2dd2329afdace52f76713aa7b71dcc448e357685
Daniel Laurindo <dslaurindo@gmail.com>
2018-08-18 00:13:26 -0300

    Improve Celcoin lib

--- core/Celcoin.js
+++ core/Celcoin.js
@@ -1,14 +1,7 @@
 const Recharge = require('./Recharge');
 const Payment= require('./Payment');
 
-class Celcoin {
-    contructor() {}
-    recharge() {
-        return new Recharge();
-    }
-    payment() {
-        return new Payment();
-    }
-}
-
-module.exports = Celcoin;
+module.exports = {
+    Recharge,
+    Payment
+};
