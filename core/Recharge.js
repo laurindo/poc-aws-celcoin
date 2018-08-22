177c4dfb851dd1164746b9929787fe1984552b5a
Daniel Laurindo <dslaurindo@gmail.com>
2018-08-22 00:15:20 -0300

    Removing operationDate param from getStatus Recharge

--- core/Recharge.js
+++ core/Recharge.js
@@ -159,9 +159,8 @@ class Recharge {
 
     /**
      * @param {string} protocoloId        - 2143935
-     * @param {iso date} operationDate    - 2018-08-21T23:09:22
     */
-    getStatus(protocoloId, operationDate) {
+    getStatus(protocoloId) {
         const self = this;
         return new Promise((resolve, reject) => {
             const wsdlUri = self.WebServerUtils.getWSDL_URI();
@@ -171,7 +170,7 @@ class Recharge {
                     ...self.WebServerUtils.getTransactionArgs('TransacaoStatusOperacao', 'CONSULTASTATUS'),
                     EnderecoIP: '127.0.0.1',
                     DadosConsultaOperacao: {
-                        DataOperacao: operationDate,
+                        DataOperacao: '0001-01-01T00:00:00',
                         ProtocoloId: protocoloId
                     }
                 }
